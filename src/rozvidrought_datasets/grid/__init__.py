c257e930835c81e1bbca1b546d9de19889b98146
Simon <simon@ramangwana.earth>
2026-03-13 18:05:43 +0300

    Add Rozvi grid specification constants

---
+++ src/rozvidrought_datasets/grid/__init__.py
@@ -0,0 +1,14 @@
+from .spec import (
+    CRS,
+    WIDTH,
+    HEIGHT,
+    TOTAL_CELLS,
+    PIXEL_SIZE_X,
+    PIXEL_SIZE_Y,
+    ORIGIN_X,
+    ORIGIN_Y,
+    LEFT,
+    RIGHT,
+    BOTTOM,
+    TOP,
+)
